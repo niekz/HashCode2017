8efd6a0979fdfb66b94cf76698fd1df22c552593
johannventer2@gmail.com <rand0m>
2017-02-24 16:11:41 +0200

    Initial Commit for main file - Added TODO's for various sections

---
+++ src/main.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by johann on 2017/02/24.
+  */
+object main {
+  def main(args: Array[String]): Unit = {
+    //TODO: Parse input
+    //TODO: Add basic round robin case
+    //TODO: Elim possible redundant cache servers
+    //TODO: Elim DataCentre Latency < Cache Server Latency
+    //TODO: Take a probability approach to locate most likely cache server for video (based on request # and *latency)
+  }
+}
